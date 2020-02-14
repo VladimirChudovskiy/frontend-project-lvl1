715b77200f80d2241d31116919d716bd4ae889d4
vchudovskiy <vchudovskiy.itg@gmail.com>
2020-02-14 16:48:27 +0200

    added greeting function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import readlineSync from 'readline-sync';
 
+const firstGreeting = () => {
+    console.log('Welcome to the Brain Games!');
+
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+};
+
+export {
+    firstGreeting,
+};
